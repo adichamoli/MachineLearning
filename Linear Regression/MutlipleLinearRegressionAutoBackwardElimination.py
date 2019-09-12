77d1aa8cc75303251b8e654a10fe6b4abaf0ebb2
adichamoli <50514555+adichamoli@users.noreply.github.com>
2019-09-12 21:49:15 +0530

    Machine Learning Project

--- Linear Regression/MutlipleLinearRegressionAutoBackwardElimination.py
+++ Linear Regression/MutlipleLinearRegressionAutoBackwardElimination.py
@@ -37,6 +37,30 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import statsmodels.api as sm
 
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
 '''Importing the Dataset'''
 dataset = pd.read_csv('50_Startups.csv')
 print(dataset.head())
@@ -66,28 +90,8 @@ y_pred = regressor.predict(X_test)
 print(y_pred)
 
 '''Building the optimal model using Backward Elimination'''
+SL = 0.05
 X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-
-'''Repeating Step 3 of Backward Elimination to get P > 0.05'''
-X_opt = X[:, [0, 1, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-
-X_opt = X[:, [0, 1, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-
-X_opt = X[:, [0, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-
-X_opt = X[:, [0, 3, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-
-X_opt = X[:, [0, 3]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
+X_Modeled = backwardElimination(X_opt, SL)
+print(X_Modeled.summary())
