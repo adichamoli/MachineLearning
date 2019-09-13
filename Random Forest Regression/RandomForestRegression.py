0a7cd6745d0c60f35cebb5fe260b797003ef3790
adichamoli <50514555+adichamoli@users.noreply.github.com>
2019-09-13 16:24:56 +0530

    Machine Learning Project

--- Random Forest Regression/RandomForestRegression.py
+++ Random Forest Regression/RandomForestRegression.py
@@ -2,8 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 #Importing the Dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -16,19 +15,19 @@ y = dataset.iloc[:,2].values
 #Since Data is small, so whole model is needed
 
 '''Fitting SVR to Dataset'''
-regressor = DecisionTreeRegressor(random_state = 0)
+regressor = RandomForestRegressor(n_estimators=300, random_state=0)
 regressor.fit(X, y)
 
 '''Predicting a new result'''
 y_pred = regressor.predict([[6.5]])
 print(y_pred)
 
-'''Visualizing the Decision Tree Regression Results'''
+'''Visualizing the Random Forest Regression Results'''
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color='red')
 plt.plot(X_grid, regressor.predict(X_grid), color='blue')
-plt.title('Truth or Bluff Decision Tree Regression')
+plt.title('Truth or Bluff Random Forest Regression')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
